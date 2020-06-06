fdd9ca8c74a1ade2d0bf41831ed7c082c237ee47
Rachel Kat <rachelkat@gmail.com>
2020-06-06 19:13:58 -0400

    installed material ui core and icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-function Nav({ name, location }) {
-  return <div>
-    <h1>Any recommendations for {name} near {location}?</h1>
-  </div>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+export default function Nav({ name, location }) {
+  const classes = useStyles();
+  return (
+    <>
+      <h1>Any recommendations for {name} near {location}?</h1>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <TextField
+        placeholder="Placeholder"
+        label="TextBox" />
+    </>
+  )
 }
-
-export default Nav;
