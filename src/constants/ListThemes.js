829d7b6b794a7cb80238e9c6c3ce3ca81df9bef8
Ariane <ariane.fairlie@gmail.com>
2020-06-08 15:06:24 -0400

    moved listThemes to separate doc. Bug: zindex broken unless style declared in List.js

---
+++ src/constants/ListThemes.js
@@ -0,0 +1,27 @@
+import { createMuiTheme, makeStyles } from "@material-ui/core/styles";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    zIndex: 900
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    zIndex: 900
+  },
+  drawerContainer: {
+    overflow: 'auto'
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+export default useStyles;
