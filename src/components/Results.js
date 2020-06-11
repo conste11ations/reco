bb9e6d01c30de8c5619682ac9c132c2e0d9e9c77
Rachel Kat <rachelkat@gmail.com>
2020-06-10 20:22:19 -0400

    add key to results component

--- src/components/Results.js
+++ src/components/Results.js
@@ -10,7 +10,7 @@ export default function Results(props) {
   if (results === undefined) { return <div></div> };
   return results.map(result => {
     return (
-      <div
+      <div key={result.id}
         className={classes.resultItem}
         onClick={() => { setValue(""); setPlaceholder(result[returnVal]); setResultId(result.id)}}>
         {result[returnVal]}
