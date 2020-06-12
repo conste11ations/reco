ecd0ac40ee9b6ad33a061af52a8715eb3877606e
Ariane <ariane.fairlie@gmail.com>
2020-06-11 21:13:12 -0400

    cleanup mui imports

--- src/components/List/RecommendationCard.js
+++ src/components/List/RecommendationCard.js
@@ -1,24 +1,15 @@
 import React, {useState, Fragment} from 'react';
 import axios from 'axios';
 
-import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
+
+import { makeStyles } from '@material-ui/core/styles';
+import {Card, CardHeader, CardMedia, CardContent, CardActions, Collapse, IconButton, Typography, List, ListItem, Link, Button} from '@material-ui/core';
+
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem'
-import Link from '@material-ui/core/Link';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
-import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles((theme) => ({
   root: {
