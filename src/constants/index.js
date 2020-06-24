b87f00743af251f60b81f61a11e8230c87f737f6
Rachel Kat <rachelkat@gmail.com>
2020-06-24 02:31:52 -0400

    back to localhost

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const API_ROOT = 'https://lhl-reco-api.herokuapp.com/api';
-export const API_WS_ROOT = 'ws://lhl-reco-api.herokuapp.com/cable';
+export const API_ROOT = 'http://localhost:3001/api';
+export const API_WS_ROOT = 'ws://localhost:3001/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
