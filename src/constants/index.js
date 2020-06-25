c4dfd881a31fb22d9505903254df4f48e7bb9eef
Rachel Kat <rachelkat@gmail.com>
2020-06-24 23:32:36 -0400

    trying changing to wss

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const API_ROOT = 'https://lhl-reco-api.herokuapp.com/api';
-export const API_WS_ROOT = 'ws://lhl-reco-api.herokuapp.com/cable';
+export const API_WS_ROOT = 'wss://lhl-reco-api.herokuapp.com/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
