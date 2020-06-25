76bd63dc8d9c621302688f20c07dd7b71adcc124
Rachel Kat <rachelkat@gmail.com>
2020-06-25 00:38:24 -0400

    trying width again in search

--- src/constants/searchThemes.js
+++ src/constants/searchThemes.js
@@ -21,12 +21,12 @@ const useSearchStyle = makeStyles(theme => ({
       backgroundColor: fade(theme.palette.common.white, 0.75)
     },
     marginRight: 0,
-    marginLeft: "50px",
+    marginLeft: 0,
     width: "100%",
     [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(1),
       marginRight: theme.spacing(1),
-      width: "auto"
+      width: "260px"
     }
   },
   searchIcon: {
